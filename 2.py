ed5430193cd8a5e1cfffe205c58e2a28506727a2
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2020-08-03 20:38:23 +0530

    with noise factor 0.25

--- 2.py
+++ 2.py
@@ -2,20 +2,19 @@
 """
 Created on Mon Aug  3 13:56:25 2020
 
-@author: trup3943
+@author: Thejan Rupasinghe
 """
 
-# main imports
+# library imports
 import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, Dropout
+from keras.activations import relu, sigmoid
+from keras import utils, datasets
 
-# Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+# load dataset - test and train
+(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 # add noise to train and test images
 noise_factor = 0.25
@@ -24,45 +23,38 @@ x_test = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_tes
 x_train = np.clip(x_train, 0., 1.)
 x_test = np.clip(x_test, 0., 1.)
 
-# Scale images to the [0, 1] range
+# convert to float32 and scale variables to 0-1 range - 255 is the max value for a grey scale pixel
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
+
+# images (28,28) by initial and 1 channel as grey scale 2D, Therefore (28, 28, 1)
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+print("x_train Shape:", x_train.shape)
+print("Training data:", x_train.shape[0])
+print("Testing data:", x_test.shape[0])
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+# convert label (dependent variable) to binary one-hot - to use softmax in NN
+y_train = utils.to_categorical(y_train, 10)
+y_test = utils.to_categorical(y_test, 10)
 
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
+# buliding the model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(10, activation="softmax"))
 
+# print summary  
 model.summary()
 
-batch_size = 128
-epochs = 15
-
+# compile the model
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+# train the model
+model.fit(x_train, y_train, batch_size=128, epochs=15, validation_split=0.1)
 
+# test the model
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
-
-
- 
